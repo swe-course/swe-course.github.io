7b860c1bd4d3cece01535f2826de45ee778484cc
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2019-12-18 13:37:02 +0200

    v19.12.0

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route/*, Link, Redirect*/ } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
